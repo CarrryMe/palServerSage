f3ece2c8f7bae011eb3620f1b871f681fee0342a
GodInDream <zp@1105.my>
2024-02-18 14:14:17 +0800

    内存检测不足重启

--- job.py
+++ job.py
@@ -3,10 +3,16 @@ import SageServer
 import schedule
 import time
 
+def save_back():
+    SageServer.save_back(False)
+
+def available():
+    SageServer.available()
+
 # 设置每 5 分钟执行一次任务
-schedule.every(5).minutes.at(":00").do(SageServer.save_back)
+schedule.every(1).minutes.at(":00").do(save_back)
 # 设置每 5 分钟执行一次任务
-schedule.every(5).minutes.at(":00").do( SageServer.available)
+schedule.every(5).minutes.at(":00").do(available)
 
 def start_job():
     # 主循环，保持程序运行
